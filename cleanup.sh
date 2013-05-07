65d5b23a76ee88209b3a8dc0669dff1b2f56a74b
Matthias Braun <matthias.braun@kit.edu>
2013-05-07 13:59:31 +0200

    remove rel='nofollow'

--- cleanup.sh
+++ cleanup.sh
@@ -5,5 +5,5 @@ for i in templates/*; do
 	fi
 	# remove [edit] links
 	cp $i /tmp/tmpfoo
-	cat /tmp/tmpfoo | perl -pe 's|<span class="editsection">.*?</span> ?||' < /tmp/tmpfoo | perl -pe 's|<a href="/firm/|<a href="|' | perl -pe 's|<img alt="([^"]*)" src="/firmw/images/[^"]*/(.*)"|<img alt="\1" src="images/\2"|' > $i
+	cat /tmp/tmpfoo | perl -pe 's|<span class="editsection">.*?</span> ?||' < /tmp/tmpfoo | perl -pe 's|<a href="/firm/|<a href="|' | perl -pe 's|<img alt="([^"]*)" src="/firmw/images/[^"]*/(.*)"|<img alt="\1" src="images/\2"|' | perl -pe 's| rel="nofollow"||g' > $i
 done
